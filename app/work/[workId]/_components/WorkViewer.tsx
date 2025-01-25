5c9d87baf7e087c2730ea1c5a5a043dfe7c11b3d
Ogikusa <ikw.haruto1204@gmail.com>
2025-01-25 14:06:28 +0900

    Fixed

--- app/work/[workId]/_components/WorkViewer.tsx
+++ app/work/[workId]/_components/WorkViewer.tsx
@@ -1,4 +1,4 @@
-import { Typography } from "@mui/material";
+import { Box, Link, Stack, Typography } from "@mui/material";
 import FileWorkViewer from "./FileWorkViewer";
 import TextWorkViewer from "./TextWorkViewer";
 import LyricsWorkViewer from "./LyricsWorkViewer";
@@ -14,5 +14,26 @@ export default function WorkViewer({
   if (workType === WorkType.FILES) return <FileWorkViewer workId={workId} />;
   if (workType === WorkType.TEXT) return <TextWorkViewer workId={workId} />;
   if (workType === WorkType.LYRICS) return <LyricsWorkViewer workId={workId} />;
-  return <Typography>（対応していない作品タイプです）</Typography>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "90vh",
+        width: "100%",
+      }}
+    >
+      <Stack direction="column" spacing={2} alignItems="center">
+        <Typography variant="h2" sx={{ fontSize: "2em" }}>
+          対応していない作品タイプです
+        </Typography>
+        <Typography sx={{ textAlign: "center", textWrap: "balance" }}>
+          現在、この作品タイプは対応していません。アップデート等により変更された可能性があります。
+          <br />
+          詳しくは Github レポジトリの Migration を参照してください
+        </Typography>
+      </Stack>
+    </Box>
+  );
 }
